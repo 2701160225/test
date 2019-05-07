7db3802231fd010767106694977952c961704805
2701160225 <731652732@qq.com>
2019-05-07 20:33:04 +0800

    第二次测试

---
+++ src/demo1/demo02.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import java.util.Scanner;
+
+public class demo02 {
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String s= "this is demo01";
+		Scanner cin = new Scanner(System.in); 
+		s = cin.next();
+		System.out.println(s);
+	}
+	
+}
